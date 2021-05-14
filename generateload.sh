4d43129da11391566c401ae55bf4f6044467587e
Paul Kukiel <kukiel@amazon.com>
2021-05-14 16:04:30 +1000

    More mess

--- generateload.sh
+++ generateload.sh
@@ -9,15 +9,25 @@ for i in $(seq 1 $COUNT); do
     sleep .1
     #curl --location --request GET 'https://ut2stg6wge.execute-api.ap-southeast-2.amazonaws.com/Prod/lists' --header 'Authorization: Bearer 12345'
 
-    result=$(curl -s --location --request GET 'https://brirwhx7ue.execute-api.ap-southeast-2.amazonaws.com/Prod/lists' --header 'Authorization: Bearer 12345')
+    result=$(curl -s --location --request GET 'https://wqk1lztjea.execute-api.ap-southeast-2.amazonaws.com/Prod/lists' --header 'Authorization: Bearer 12345')
 
     echo "${result}" | jq '.[] .listId' |
     while IFS=$"\n" read -r c; do
         list=$(echo "$c" | sed 's/"//g')
-        url="curl -s --location --request GET 'https://brirwhx7ue.execute-api.ap-southeast-2.amazonaws.com/Prod/lists/$list' --header 'Authorization: Bearer 12345'"
+        url="curl -s --location --request GET 'https://wqk1lztjea.execute-api.ap-southeast-2.amazonaws.com/Prod/lists/$list' --header 'Authorization: Bearer 12345'" 
         r=$(eval $url)
         echo "${r}" | jq
+        curl -s --location --request GET 'https://wqk1lztjea.execute-api.ap-southeast-2.amazonaws.com/Prod/wavefile' &
     done
+    #n=$(( ( RANDOM % 10 )  + 1 ))
+    #urlFib="curl -s --location --request GET 'https://wqk1lztjea.execute-api.ap-southeast-2.amazonaws.com/Prod/fib/$n'"
+    #echo $urlFib
+    
+    curl -s --location --request GET 'https://wqk1lztjea.execute-api.ap-southeast-2.amazonaws.com/Prod/fib/'$(( ( RANDOM % 10 )  + 1 )) &
+    
+    result=$(curl -s --location --request GET 'https://wqk1lztjea.execute-api.ap-southeast-2.amazonaws.com/Prod/check' --header 'Authorization: Bearer 12345')
+    echo "${result}" | jq 
+
 done
 
 END_DATETIME=$(date +%s)
